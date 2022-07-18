1ac815d97e08df3e73e12abeed40192e5a72fa4f
TheTechromancer <thetechromancer@protonmail.com>
2022-07-18 13:31:41 -0400

    generate .mp4 directly

--- README.md
+++ README.md
@@ -4,33 +4,31 @@ Art display with seizure-inducing glitch transitions.
 https://user-images.githubusercontent.com/20261699/127774463-903d5583-5281-4187-ba1e-d2a44b14e9c0.mp4
 
 ## Installation
-~~~
-$ pip install git+https://github.com/thetechromancer/glitch-art-display
+~~~bash
+pip install git+https://github.com/thetechromancer/glitch-art-display
 ~~~
 
 ## Example
 NOTE: All images must be the same resolution
+~~~bash
+# generate .MP4
+glitch-art-display ~/Pictures ./output.mp4
 
-NOTE: Frames in the `OUTPUT` directory are symlinks only, so they take up very little space.
-~~~
-# generate frames
-$ glitch-art-display --shuffle ~/Pictures /tmp/ffmpeg_frames
-
-# generate .MP4 from frames
-$ ffmpeg -framerate 25 -i /tmp/ffmpeg_frames/frame_%09d.png output.mp4
-
-# loop forever
-$ vlc --loop --fullscreen --no-video-title-show output.mp4
+# play the video and loop forever
+vlc --loop --fullscreen --no-video-title-show output.mp4
 ~~~
 
 ## Usage
-~~~
+~~~bash
+glitch-art-display --help
 Usage: glitch-art-display [OPTIONS] INPUT OUTPUT
 
 Options:
-  --amount INTEGER RANGE       Glitch amount (1 to 100)
-  --normal-frames INTEGER      Number of normal frames
-  --transition-frames INTEGER  Number of glitchy transition frames
-  --shuffle                    Shuffle order of images
-  --help                       Show this message and exit.
+  --amount INTEGER RANGE       Glitch amount  [default: 50; 1<=x<=100]
+  --fps FLOAT                  Frames per second  [default: 25]
+  --normal-frames INTEGER      Number of normal frames  [default: 625]
+  --transition-frames INTEGER  Number of glitchy transition frames  [default:
+                               30]
+  --dont-shuffle               Don't shuffle order of images  [default: False]
+  --help                       Show this message and exit.  [default: False]
 ~~~
--- glitch_art_display/main.py
+++ glitch_art_display/main.py
@@ -3,12 +3,14 @@
 import os
 import click
 import hashlib
+import imageio
 from .jpeg import *
 import subprocess as sp
 from pathlib import Path
+from contextlib import suppress
 from multiprocessing import cpu_count
 
-from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 
 cache_dir = Path.home() / '.cache/glitch-art-display'
 cache_dir.mkdir(exist_ok=True)
@@ -49,11 +51,11 @@ def find_images(directory):
                     print(f'[!]  - please install imagemagick in order to use {filename.suffix} files')
 
 
-def gen_frames(image_dir, output, glitch_amount=100, num_image_frames=25, num_transition_frames=30, shuffle=False):
+def gen_frames(image_dir, output, glitch_amount=100, fps=25, num_image_frames=25, num_transition_frames=30, shuffle=False):
 
     frame_groups = []
-    frames_output = Path(output)
-    frames_output.mkdir(exist_ok=True)
+    frames_output = Path(output).resolve()
+    frames_output.parent.mkdir(parents=True, exist_ok=True)
     frame_number = 0
 
     transition_frames = int(num_transition_frames/2)
@@ -61,52 +63,56 @@ def gen_frames(image_dir, output, glitch_amount=100, num_image_frames=25, num_tr
     if shuffle:
         random.shuffle(found_images)
 
-    for image in found_images:
 
-        print(f'[+] Generating frames for {image.name}')
+    with concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count()) as pool:
+        for image in found_images:
+            try:
+                print(f'[+] Generating frames for {image.name}')
 
-        glitched_frames = []
+                glitched_frames = []
 
-        with ThreadPoolExecutor(max_workers=cpu_count()) as pool:
+                image_bytes = bytearray(image.read_bytes())
+                image_hash = hashlib.md5(image_bytes).hexdigest()
+                png_filename = cache_dir / f'{image_hash}.png'
+                if not png_filename.exists():
+                    pool.submit(sp.run, ['convert', str(image), str(png_filename)], check=True)
 
-            image_bytes = bytearray(image.read_bytes())
-            image_hash = hashlib.md5(image_bytes).hexdigest()
-            png_filename = cache_dir / f'{image_hash}.png'
-            pool.submit(sp.run, ['convert', str(image), str(png_filename)], check=True)
+                # generate glitch frames
+                glitched_futures = []
+                for i in range(transition_frames):
+                    amount = int(glitch_amount * ((i+1)/transition_frames))
+                    glitched_futures.append(pool.submit(glitch, image, amount, i))
 
-            # generate glitch frames
-            glitched_futures = []
-            for i in range(transition_frames):
-                amount = int(glitch_amount * ((i+1)/transition_frames))
-                glitched_futures.append(pool.submit(glitch, image, amount, i))
+                concurrent.futures.wait(glitched_futures)
+                for glitched_future in glitched_futures:
+                    glitched_frame = glitched_future.result()
+                    if glitched_frame:
+                        glitched_frames.append(glitched_frame)
+            except KeyboardInterrupt:
+                pool.shutdown(wait=False, cancel_futures=True)
+                raise
 
-            pool.shutdown(wait=True)
-            for glitched_future in glitched_futures:
-                glitched_frame = glitched_future.result()
-                if glitched_frame:
-                    glitched_frames.append(glitched_frame)
+            glitch_in = []
+            random.shuffle(frame_timings)
+            for i, glitched_image in enumerate(glitched_frames[::-1]):
+                glitch_group = []
+                for j in range(frame_timings[i % len(frame_timings)]):
+                    glitch_group.append(glitched_image)
+                glitch_in.append(glitch_group)
 
-        glitch_in = []
-        random.shuffle(frame_timings)
-        for i, glitched_image in enumerate(glitched_frames[::-1]):
-            glitch_group = []
-            for j in range(frame_timings[i % len(frame_timings)]):
-                glitch_group.append(glitched_image)
-            glitch_in.append(glitch_group)
+            normal = []
+            for i in range(num_image_frames):
+                normal.append(png_filename)
 
-        normal = []
-        for i in range(num_image_frames):
-            normal.append(png_filename)
+            glitch_out = []
+            random.shuffle(frame_timings)
+            for i, glitched_image in enumerate(glitched_frames):
+                glitch_group = []
+                for j in range(frame_timings[i % len(frame_timings)]):
+                    glitch_group.append(glitched_image)
+                glitch_out.append(glitch_group)
 
-        glitch_out = []
-        random.shuffle(frame_timings)
-        for i, glitched_image in enumerate(glitched_frames):
-            glitch_group = []
-            for j in range(frame_timings[i % len(frame_timings)]):
-                glitch_group.append(glitched_image)
-            glitch_out.append(glitch_group)
-
-        frame_groups.append([glitch_in, normal, glitch_out])
+            frame_groups.append([glitch_in, normal, glitch_out])
 
     frames = []
     interlace_frames = max(2, min(int(transition_frames/4), 1))
@@ -130,12 +136,25 @@ def gen_frames(image_dir, output, glitch_amount=100, num_image_frames=25, num_tr
                 frames += glitch_next_interlaced[i]
                 frames += glitch_interlaced[i]
 
-    for frame_number, frame in enumerate(frames):
-        frame_name = frames_output / f'frame_{frame_number:09}.png'
-        frame_name.unlink(missing_ok=True)
-        frame_name.symlink_to(frame)
+    w = imageio.get_writer(str(frames_output), format='FFMPEG', mode='I', fps=fps, macro_block_size=1)
+    try:
+        prev_frame = ''
+        for i, frame in enumerate(frames):
+            print(f'\r[+] Rendering frame {i:,}/{len(frames):,} ({i/len(frames)*100:.1f}%)', end='')
+            frame = str(frame)
+            if frame != prev_frame:
+                read_frame = imageio.imread(frame, pilmode='RGB')
+            try:
+                w.append_data(read_frame)
+            except ValueError:
+                print(f'[!] {frame}')
+                raise
+            prev_frame = str(frame)
+    finally:
+        with suppress(Exception):
+            w.close()
 
-    print('[+] All frames generated.')
+    print(f'[+] Video saved to {frames_output}')
 
 
 def glitch(image, amount=None, sequence=''):
@@ -189,19 +208,21 @@ def glitch(image, amount=None, sequence=''):
 
 
 def main():
-    @click.command()
+    @click.command(context_settings={'show_default': True})
     @click.argument('input', required=True, type=click.Path(exists=True, file_okay=False, dir_okay=True))
-    @click.argument('output', required=True, type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True))
-    @click.option('--amount', default=50, type=click.IntRange(1, 100, clamp=True), help='Glitch amount (1 to 100)')
+    @click.argument('output', required=True, type=click.Path(dir_okay=False))
+    @click.option('--amount', default=50, type=click.IntRange(1, 100, clamp=True), help='Glitch amount')
+    @click.option('--fps', default=25, type=float, help='Frames per second')
     @click.option('--normal-frames', default=25*25, type=int, help='Number of normal frames')
     @click.option('--transition-frames', default=30, type=int, help='Number of glitchy transition frames')
-    @click.option('--shuffle', is_flag=True, help='Shuffle order of images')
-    def go(input, output, amount, normal_frames, transition_frames, shuffle):
-        gen_frames(input, output, amount, normal_frames, transition_frames, shuffle)
-        click.echo(f"[+] Example: generate .MP4 with ffmpeg")
-        click.echo(f"ffmpeg -framerate 25 -i {Path(output) / 'frame_%09d.png'} /tmp/output.mp4")
+    @click.option('--dont-shuffle', is_flag=True, help='Don\'t shuffle order of images')
+    def go(input, output, amount, fps, normal_frames, transition_frames, dont_shuffle):
+        gen_frames(input, output, amount, fps, normal_frames, transition_frames, not dont_shuffle)
 
-    go()
+    try:
+        go()
+    except KeyboardInterrupt:
+        print("Interrupted")
 
 if __name__ == '__main__':
     main()
